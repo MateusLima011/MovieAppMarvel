6f3bb1dccb747ac0a8d1b3f8bf97d770babe2682
MateusLima <36885785+MateusLima011@users.noreply.github.com>
2022-06-01 18:24:59 -0300

    created drawerlayout e menu na activity main

--- app/src/main/java/com/mgt/movieappmarvel/MainActivity.kt
+++ app/src/main/java/com/mgt/movieappmarvel/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.mgt.movieappmarvel
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerToggle: ActionBarDrawerToggle
+    private lateinit var toolbar: Toolbar
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.drawer_activity)
+        toolbar = findViewById(R.id.toolbar_activity)
+
+        setSupportActionBar(toolbar)
+
+        drawerToggle = ActionBarDrawerToggle(this, drawerLayout,toolbar,
+        R.string.open,R.string.close)
+
+        drawerToggle.isDrawerIndicatorEnabled = true
+        drawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
     }
 }
